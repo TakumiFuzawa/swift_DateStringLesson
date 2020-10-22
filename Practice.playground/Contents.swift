e9ba3d5fe51249eff02d9485b8a63bff1332f20b
Fuzawa <futaku0811@gmail.com>
2020-10-22 17:45:46 +0900

    first commit

--- Practice.playground/Contents.swift
+++ Practice.playground/Contents.swift
@@ -1,14 +1,42 @@
 import UIKit
+//言語と地域の設定
+let locale = Locale(identifier: "en_US_POSIX")
+//時差の設定
+let timezone = TimeZone(abbreviation: "JST")
+
+//Lesson1
+let dateformatter = DateFormatter()
+dateformatter.dateFormat = "yyyy-MM-dd"
+dateformatter.locale = locale
+dateformatter.timeZone = timezone
+let date1 = dateformatter.date(from: "2020-08-11")!
+print(date1)
+
+//Lesson2
+let dateformatter2 = DateFormatter()
+dateformatter2.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
+dateformatter2.locale = locale
+dateformatter2.timeZone = timezone
+
+let date2 = dateformatter2.date(from: "2020-08-28T06:52:30.000Z")!
+print(date2)
+
+//Lesson3
+//現在の時刻
+let today = Date()
+let dateformatter3 = DateFormatter()
+let target = Calendar.current.dateComponents([.day], from: date1, to: today).day!
+print("\(target)日前")
+
+//Lesson4
+let dateformatter4 = DateFormatter()
+dateformatter4.dateFormat = "yyyy年MM月dd日"
+dateformatter4.locale = locale
+dateformatter4.timeZone = timezone
+
+let date4 = dateformatter4.string(from: date1)
+print(date4)
 
 
-//Lesson1 円周率 3.14159265359を少数第２位まで表示するコードを書く
-let pi = 3.14159265359
-let piNumber = round(pi*10) / 10
-print(piNumber)
 
-//Lesson2 整数型4535800を、３桁区切りのカンマをを付けて4,535,800と表示するコードを書く
-let formatter = NumberFormatter()
-//スタイル指定
-formatter.numberStyle = .decimal
-//数値の区切り文字を指定
-formatter.groupingSeparator = ","
+
